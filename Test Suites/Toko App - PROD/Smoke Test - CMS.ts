dd207df5733ef08b64e559d3f8aa97d89e14689e
minangarmarin <minang@dbo.id>
2022-10-20 14:38:19 +0700

    Edit Smoke Test Suit

--- Test Suites/Toko App - PROD/Smoke Test - CMS.ts
+++ Test Suites/Toko App - PROD/Smoke Test - CMS.ts
@@ -11,8 +11,8 @@
          <configuration>
             <groupName>Web Desktop</groupName>
             <profileName>default</profileName>
-            <requireConfigurationData>false</requireConfigurationData>
-            <runConfigurationId>Chrome</runConfigurationId>
+            <requireConfigurationData>true</requireConfigurationData>
+            <runConfigurationId>TestCloud</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/Toko App - PROD/Smoke Test - CMS Admin Verifikasi</testSuiteEntity>
